b81eab8297adc814f267867e046a306d8a68f01e
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-15 19:32:41 -0300

    fix user

--- src/server/trackapi/Model/User.cs
+++ src/server/trackapi/Model/User.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using trackapi.Config;
 
 namespace trackapi.Model
