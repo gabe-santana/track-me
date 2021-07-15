41235fb212f6b1025b9dbabc07c09a5691112cc3
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-07-15 11:33:52 -0300

    BsonCollectionAttribute added

---
+++ src/server/trackapi/Config/BsonCollectionAttribute.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace trackapi.Config
+{
+   [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
