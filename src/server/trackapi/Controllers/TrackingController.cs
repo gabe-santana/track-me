3b128adfec7edd169c92445ca6c3cbf959a5eab6
Gabriel Santana <gabe.santana.souza@gmail.com>
2021-08-01 14:02:30 -0300

    Removing echo private method

--- src/server/trackapi/Controllers/TrackingController.cs
+++ src/server/trackapi/Controllers/TrackingController.cs
@@ -54,25 +54,5 @@ namespace trackapi.Controllers
               HttpContext.Response.StatusCode = 400;
         }
 
-        private async Task Echo(WebSocket webSocket)
-        {
-            var buffer = new byte[1024 * 4];
-            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            _logger.Log(LogLevel.Information, "Message received from Client");
-
-            while (!result.CloseStatus.HasValue)
-            {
-                var serverMsg = Encoding.UTF8.GetBytes($"Server: Hello. You said: {Encoding.UTF8.GetString(buffer)}");
-                await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
-                _logger.Log(LogLevel.Information, "Message sent to Client");
-
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                _logger.Log(LogLevel.Information, "Message received from Client");
-                
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-            _logger.Log(LogLevel.Information, "WebSocket connection closed");
-        }
-
     }   
 }
